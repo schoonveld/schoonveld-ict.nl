bd3c760d7d763497377ef05333b9b96c7f59d61b
Steffen <elogio.inschrijvingen@gmail.com>
2025-04-18 16:23:54 +0200

    First minimal setup with simple homepage and resume link.

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import pluginAstro from "eslint-plugin-astro";
+import prettier from "eslint-config-prettier";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  ...pluginAstro.configs.recommended,
+  prettier,
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
+  { rules: {
+    "no-console": "warn",
+      "quotes": "error"
+    }}
+
+]);
